b63e01691dfa2962ee6fbcb66f8f210da71869b1
Tobias Punke <tobias@punke.eu>
2018-03-27 07:40:39 +0200

    Standard 2.0

---
+++ src/Logixware.Build.Tasks.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+
+// Copyright © 2006 Paul Welter
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+// [assembly: AssemblyTitle("MSBuild.Community.Tasks")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © 2008 - 2018 Tobias Punke. All rights reserved.")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyTitle("Logixware© Build Tasks Library")]
+[assembly: AssemblyDescription("Logixware© Build Tasks Library")]
+[assembly: AssemblyCompany("Logixware© Tobias Punke")]
+[assembly: AssemblyTrademark("Logixware© Tobias Punke")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5778834C-159E-48B7-B5DE-B81A9E99CDDD")]
